0d5740c47e58195bb8e5f14d135ba1d1e469f273
Marcus Loiseau <marcusloiseau1@gmail.com>
2018-11-19 16:13:10 -0300

    finished validations for the controllers

--- app/models/laundromat.rb
+++ app/models/laundromat.rb
@@ -1,4 +1,7 @@
 class Laundromat < ApplicationRecord
   has_many :users
-  has_many :reviews, through: :orders
+  has_many :reviews, through: :orders, source: :user
+  validates :name, :address, :phone_number, :bags_per_day, presence: true
+  validates :address, presence: true
+  validates phone_number, presence: true
 end
