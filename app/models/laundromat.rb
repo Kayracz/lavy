ac0b28c289e62315c42474c2310397ec1f7fc4a3
Antonio Griman <42849274+ajgriman@users.noreply.github.com>
2018-11-19 16:22:52 -0300

    Update laundromat.rb

--- app/models/laundromat.rb
+++ app/models/laundromat.rb
@@ -3,5 +3,5 @@ class Laundromat < ApplicationRecord
   has_many :reviews, through: :orders, source: :user
   validates :name, :address, :phone_number, :bags_per_day, presence: true
   validates :address, presence: true
-  validates phone_number, presence: true
+  validates :phone_number, presence: true
 end
