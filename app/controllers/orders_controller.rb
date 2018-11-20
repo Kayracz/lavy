9b190be35b133567d043c687b0bfd1dca98f3005
Marcus Loiseau <marcusloiseau1@gmail.com>
2018-11-20 18:49:03 -0300

    pundit and maps completed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,32 +2,38 @@ class OrdersController < ApplicationController
   def new
     @laundromat = Laundromat.find(params[:laundromat_id])
     @order = Order.new
+    authorize @order
   end
 
   def show
-   @order = Order.find(params[:id])
- end
+    @order = Order.find(params[:id])
+    authorize @order
+  end
 
- def create
+  def create
    @order = Order.new(order_params)
    @laundromat = Laundromat.find(params[:laundromat_id])
    @order.laundromat = @laundromat
    @order.user = current_user
    if @order.save
+    authorize @order
     redirect_to laundromat_order_path(@laundromat, @order)
   else
     render :new
   end
- end
+end
 
 def edit
+ authorize @order
 end
 
 def update
+ authorize @order
 end
 
 private
 
 def orders_params
+  authorize @order
 end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
+    authorize @user
     @user = current_user
     @orders = current_user.orders
   end
