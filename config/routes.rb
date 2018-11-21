6543c65fbc3dbdcb531284e2eae772e4f7f2dfd1
Kayra Czerniewicz <kayracz@gmail.com>
2018-11-21 11:39:33 -0300

    new changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :laundromats, only: [:index, :show] do
-    resources :orders, only: [:new, :create]  # Never need a double nesting
+    resources :orders, only: [:new, :create, :show]  # Never need a double nesting
   end
 
   resources :orders, only: [:destroy, :show, :edit, :update] do
