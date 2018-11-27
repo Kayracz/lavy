f2f0992a442a8f73428ee5e092b2b8d065cda060
Marcus Loiseau <marcusloiseau1@gmail.com>
2018-11-27 10:28:58 -0300

    turning stars to rating

--- app/controllers/laundromats_controller.rb
+++ app/controllers/laundromats_controller.rb
@@ -2,50 +2,47 @@ class LaundromatsController < ApplicationController
   def index
 
     # @laundromats = @laundromats.near(params[:laundromat][:address], 5) unless params[:laundromat][:address].empty?
-
     if params[:price_cents]
       @laundromats = policy_scope(Laundromat).where("price_cents <= ?", params[:price_cents].to_i)
     else
       @laundromats = policy_scope(Laundromat)
-   end
-
-
-   if params[:distance]
-     @laundromats = @laundromats.near(Geocoder.coordinates(params[:address]), params[:distance].to_i)
-    else
-      @laundromats
-   end
-
-
-
-    @markers = @laundromats.map do |laundromat|
-      {
-        lng: laundromat.longitude,
-        lat: laundromat.latitude,
-        infoWindow: { content: render_to_string(partial: "/laundromats/map_window", locals: { laundromat: laundromat}) }
-      }
     end
+
+
+    if params[:distance]
+     @laundromats = @laundromats.near(Geocoder.coordinates(params[:address]), params[:distance].to_i)
+   else
+    @laundromats
   end
 
-  def show
-    @laundromat = Laundromat.find(params[:id])
-    @markers =
+  @markers = @laundromats.map do |laundromat|
     {
-      lng: @laundromat.longitude,
-      lat: @laundromat.latitude,
-      infoWindow: { content: render_to_string(partial: "/laundromats/map_window", locals: { laundromat: @laundromat}) }
+      lng: laundromat.longitude,
+      lat: laundromat.latitude,
+      infoWindow: { content: render_to_string(partial: "/laundromats/map_window", locals: { laundromat: laundromat}) }
     }
-
-    authorize @laundromat
   end
+end
 
-  def new
-    @laundromat = Laundromat.new
-    @laundromat.save
-    authorize @laundromat
-  end
+def show
+  @laundromat = Laundromat.find(params[:id])
+  @markers =
+  {
+    lng: @laundromat.longitude,
+    lat: @laundromat.latitude,
+    infoWindow: { content: render_to_string(partial: "/laundromats/map_window", locals: { laundromat: @laundromat}) }
+  }
 
-  def create
+  authorize @laundromat
+end
 
-  end
+def new
+  @laundromat = Laundromat.new
+  @laundromat.save
+  authorize @laundromat
+end
+
+def create
+
+end
 end
