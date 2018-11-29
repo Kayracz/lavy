f8197b45e142cee83405c067891585c1ae695029
Antonio Griman <griman@gmail.com>
2018-11-29 17:28:07 -0300

    add validation to create laundromat

--- app/models/laundromat.rb
+++ app/models/laundromat.rb
@@ -4,7 +4,7 @@ class Laundromat < ApplicationRecord
   has_many :users
   has_many :orders
   has_many :reviews, through: :orders
-  validates :name, :address, :phone_number, :bags_per_day, presence: true
+  validates :name, :address, :phone_number, :bags_per_day, :photo, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true
   monetize :price_cents
